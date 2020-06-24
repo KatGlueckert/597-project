311221ebf9749da5a3db58f2966cf2c8fe2fd572
Kat Glueckert <kaglueckert@crimson.ua.edu>
2020-06-24 09:53:56 -0500

    First Commit

---
+++ public/src/app.js
@@ -0,0 +1,11 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDOM.render(<p>This is my boilerplate </p>, document.getElementById('app'));
+
